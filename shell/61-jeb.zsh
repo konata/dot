442a915babaa6601702c0a11f06871765a3bab4e
konata <beriru@qq.com>
2026-06-30 22:11:25 +0800

    fix: harden jeb launcher

--- shell/61-jeb.zsh
+++ shell/61-jeb.zsh
@@ -1,10 +1,29 @@
 export JEB_HOME="${JEB_HOME:-$HOME/cask/jeb}"
 
+_jeb_jdk() {
+  [[ -n "${JEB_JAVA_HOME:-}" ]] && { print -r -- "$JEB_JAVA_HOME"; return; }
+
+  local jdk version
+  for jdk in "${JAVA_HOME:-}" /Library/Java/JavaVirtualMachines/*/Contents/Home(N); do
+    [[ -x "$jdk/bin/java" ]] || continue
+    version="$("$jdk/bin/java" -version 2>&1)"
+    [[ "$version" == *'version "17.'* ]] && print -r -- "$jdk" && return
+  done
+}
+
+_jeb() {
+  local jdk="$(_jeb_jdk)"
+  if [[ -n "$jdk" ]]; then
+    JAVA_HOME="$jdk" "$@"
+  else
+    "$@"
+  fi
+}
+
 jeb() {
-  "$JEB_HOME/jeb_macos.sh" "$@" >/dev/null 2>&1 &
+  _jeb "$JEB_HOME/jeb_macos.sh" "$@" </dev/null >/dev/null 2>&1 &!
 }
 
 kfc() {
-  "$JEB_HOME/jeb_macos.sh" -c --srv2 --script="$JEB_HOME/coreplugins/kfc.py"
+  _jeb "$JEB_HOME/jeb_macos.sh" -c --srv2 --script="$JEB_HOME/coreplugins/kfc.py"
 }
-
